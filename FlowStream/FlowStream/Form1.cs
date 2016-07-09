4d3ffceec81b9f611da3cfac2fda9c215f8bc7e6
Kradmoa <darkttd@gmail.com>
2016-07-09 10:28:34 +0900

    Missing project directoory change on solution; namespace change

--- FlowStream/FlowStream/Form1.cs
+++ FlowStream/FlowStream/Form1.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ScenarioGraphDrawer
+namespace FlowStream
 {
     public partial class Form1 : Form
     {
--- FlowStream/FlowStream/SoulStream/SoulStream.cs
+++ FlowStream/FlowStream/SoulStream/SoulStream.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScenarioGraphDrawer.ScenarioDrawer
+namespace FlowStream
 {
     public class SoulStream
     {
